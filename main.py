f8be0f8a24928bb225521188e7f7d0ec6d79ffb2
Kalpana <158153389+imkalpana@users.noreply.github.com>
2025-09-13 09:18:56 +0000

    load dataset the main.py
    try to read data with csv and pandas

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+import csv
+
+with open('telco_customer_churn_dataset.csv','r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        print (row)
+
+'''
+
+import pandas as pd
+
+df = pd.read_csv('telco_customer_churn_dataset.csv')
+print(df)
