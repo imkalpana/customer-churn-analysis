70ac64118e455c829a6ec3fc0dedfde20e3ac250
Kalpana <158153389+imkalpana@users.noreply.github.com>
2025-09-26 11:58:37 +0000

    add statistical summary class

--- main.py
+++ main.py
@@ -9,8 +9,28 @@ with open('telco_customer_churn_dataset.csv','r') as csvfile:
 '''
 
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('telco_customer_churn_dataset.csv')
-print(df)
+print(df.head())
+print(df.describe())
 
-# add a class to visualise to statistics 
+class ChurnPredictor:
+    def __init__(self, dataframe):
+        self.df = dataframe
+
+    def show_summary(self):
+        print("Summary Statistics:")
+        print(self.df.describe(include='all'))
+
+    def plot_churn_distribution(self):
+        sns.countplot(x='Churn', data=self.df)
+        plt.title('Churn Distribution')
+        plt.show()
+
+
+
+visualizer = ChurnPredictor(df)
+visualizer.show_summary()
+visualizer.plot_churn_distribution()
