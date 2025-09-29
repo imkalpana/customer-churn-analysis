6732a6badb529424e42d59e3fe0dd6a022911542
Kalpana <158153389+imkalpana@users.noreply.github.com>
2025-09-29 12:47:09 +0000

    add "Statistical summary and duplicate check" to commit message

--- main.py
+++ main.py
@@ -13,8 +13,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('telco_customer_churn_dataset.csv')
+
+'''
 print(df.head())
 print(df.describe())
+print(df.info())
+print(df.duplicated().sum())
+'''
+
+
 
 class ChurnPredictor:
     def __init__(self, dataframe):
@@ -29,8 +36,19 @@ class ChurnPredictor:
         plt.title('Churn Distribution')
         plt.show()
 
+    def plot_churn_distribution1(self):
+        if 'Churn' in self.df.columns:
+            churn_counts = self.df['Churn'].value_counts()
+            churn_counts.plot(kind='bar')
+            plt.title('Churn Distribution')
+            plt.xlabel('Churn')
+            plt.ylabel('Count')
+            plt.show()
+        else:
+            print("'Churn' column not found in dataset.")
+
 
 
 visualizer = ChurnPredictor(df)
-visualizer.show_summary()
-visualizer.plot_churn_distribution()
+#visualizer.show_summary()
+visualizer.plot_churn_distribution1()
